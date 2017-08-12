aaccc77d140ff0cbac679100b1ae111d821cb953
Frank Cangialosi <frankc@csail.mit.edu>
2017-08-12 07:54:39 -0400

    initial commit

---
+++ fslove.js
@@ -0,0 +1,29 @@
+/*
+Version : 1.1
+*/
+jQuery(document).ready(function($){
+    "use strict";
+
+  // Scroll down button
+  $("#scroll-icon").on('click', function() {
+     $('body').animate({scrollTop: +700}, 1000);
+  });
+
+/*
+  var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('bg-video', {});
+    }
+    // player.mute() "???"
+*/
+
+});
+
